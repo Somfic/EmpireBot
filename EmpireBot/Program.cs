f8411aba17d0e569bbbb9800081a82fcb300a376
Somfic <somfic@gmail.com>
2019-07-15 19:30:06 +0200

    Add project files.

---
+++ EmpireBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpireBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
